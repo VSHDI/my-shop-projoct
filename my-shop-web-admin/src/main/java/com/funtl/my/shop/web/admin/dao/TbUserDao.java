408f6ef0b6e55f5fa73f2711e361045cf99b8c15
VSHDI <45596499+VSHDI@users.noreply.github.com>
2019-11-26 15:23:41 +0800

    管理 | 数据层 Spring整合Mybatis及单表CRUD操作测试
    整合——mybatis-config.xml+spring-context-mybatis.xml
    Mapper.xml 管理数据操作语句

---
+++ my-shop-web-admin/src/main/java/com/funtl/my/shop/web/admin/dao/TbUserDao.java
@@ -0,0 +1,29 @@
+package com.funtl.my.shop.web.admin.dao;
+
+import com.funtl.my.shop.domain.TbUser;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TbUserDao {
+    /**
+     * 查询用户表全部信息
+     * @return
+     */
+    List<TbUser> selectAll();
+
+    void insert(TbUser tbUser);
+
+    void deleteById(Long id);
+
+    void delete(TbUser tbUser);
+
+    TbUser getById(Long id);
+
+    void update(TbUser tbUser);
+
+    List<TbUser> selectByName(String username);
+
+    TbUser selectByEmail(String email);
+}
---
+++ my-shop-web-admin/src/test/java/com/funtl/my/shop/web/admin/service/TbUserServiceTest.java
@@ -0,0 +1,78 @@
+package com.funtl.my.shop.web.admin.service;
+
+import com.funtl.my.shop.domain.TbUser;
+import com.funtl.my.shop.web.admin.dao.TbUserDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Date;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring-context.xml", "classpath:spring-context-druid.xml", "classpath:spring-context-mybatis.xml"})
+public class TbUserServiceTest {
+
+    @Autowired
+    private TbUserDao tbUserDao;
+
+    @Test
+    public void testSelectAll() {
+        List<TbUser> tbUsers = tbUserDao.selectAll();
+        for (TbUser tbUser : tbUsers) {
+            System.out.println(tbUser.getUsername());
+        }
+    }
+
+    @Test
+    public void testInsert() {
+        TbUser tbUser = new TbUser();
+        tbUser.setEmail("admin@adm2i1n.com");
+        tbUser.setPassword("adm31in");
+        tbUser.setPhone("1588888w88888");
+        tbUser.setUsername("Lusifer11w1");
+        tbUser.setCreated(new Date());
+        tbUser.setUpdated(new Date());
+
+        tbUserDao.insert(tbUser);
+    }
+
+    @Test
+    public void testDeleteById() {
+        Long id = 37L;
+        tbUserDao.deleteById(id);
+    }
+
+    @Test
+    public void testDelete() {
+        TbUser tbUser = new TbUser();
+        tbUser.setId(36L);
+
+        tbUserDao.delete(tbUser);
+    }
+
+    @Test
+    public void testGetById() {
+        TbUser tbUser = tbUserDao.getById(35L);
+        System.out.println(tbUser.getUsername());
+    }
+
+    @Test
+    public void testUpdate() {
+        TbUser tbUser = tbUserDao.getById(35L);
+        tbUser.setUpdated(new Date());
+        tbUser.setUsername("123");
+
+        tbUserDao.update(tbUser);
+    }
+
+    @Test
+    public void testSelectByName() {
+        List<TbUser> tbUsers = tbUserDao.selectByName("uni");
+        for (TbUser tbUser : tbUsers) {
+            System.out.println(tbUser.getUsername());
+        }
+    }
+}
